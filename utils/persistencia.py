8b543533b6e9ae7152881f2675971d60cfc1e6aa
Victor Contreras <vcontreras@gmail.com>
2025-05-30 13:56:09 -0300

    Enhance Persistencia class documentation with detailed method descriptions

--- utils/persistencia.py
+++ utils/persistencia.py
@@ -2,8 +2,37 @@ import pickle
 import os
 
 class Persistencia:
+    """
+    Clase utilitaria para la persistencia de datos utilizando el módulo pickle.
+    Permite guardar y cargar objetos Python en archivos binarios.
+
+    Métodos:
+    --------
+    cargar(archivo):
+        Carga y retorna los datos almacenados en el archivo especificado.
+        Si el archivo no existe o ocurre un error, retorna una lista vacía.
+
+    guardar(archivo, datos):
+        Guarda los datos proporcionados en el archivo especificado.
+        Si ocurre un error durante el guardado, muestra un mensaje por consola.
+    """
+
     @staticmethod
     def cargar(archivo):
+        """
+        Carga los datos desde un archivo utilizando pickle.
+        'rb' read binary
+
+        Parámetros:
+        -----------
+        archivo : str
+            Ruta del archivo desde donde se cargarán los datos.
+
+        Retorna:
+        --------
+        datos : object
+            Objeto cargado desde el archivo. Si ocurre un error o el archivo no existe, retorna una lista vacía.
+        """
         if os.path.exists(archivo):
             with open(archivo, 'rb') as f:
                 try:
@@ -17,6 +46,21 @@ class Persistencia:
 
     @staticmethod
     def guardar(archivo, datos):
+        """
+        Guarda los datos en un archivo utilizando pickle.
+        'wb' write binary
+
+        Parámetros:
+        -----------
+        archivo : str
+            Ruta del archivo donde se guardarán los datos.
+        datos : object
+            Objeto que se desea guardar en el archivo.
+
+        Retorna:
+        --------
+        None
+        """
         try:
             with open(archivo, 'wb') as f:
                 pickle.dump(datos, f)
